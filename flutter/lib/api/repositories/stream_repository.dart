08313802e74cf129c41a987f650555f8de023d74
Alexmz1 <menez.alexis19@gmail.com>
2026-06-08 20:38:17 +0200

    STR-37: add repositories and notifiers
    Add ModelRepository<T>, AuthRepository, StreamRepository.
    Add SessionNotifier, StreamNotifier, AudioNotifier (ChangeNotifier).
    
    Signed-off-by: Alexmz1 <menez.alexis19@gmail.com>

---
+++ flutter/lib/api/repositories/stream_repository.dart
@@ -0,0 +1,31 @@
+import '../../services/api_service.dart';
+import '../models/stream_model.dart';
+import 'model_repository.dart';
+
+class StreamRepository extends ModelRepository<StreamModel> {
+  const StreamRepository()
+      : super(
+          uri: 'streams',
+          fromJson: StreamModel.fromJson,
+        );
+
+  Future<List<StreamModel>> getActive() {
+    return ApiService().request(
+      uri: 'streams/active',
+      parser: (res) {
+        final list = res is List ? res : (res['data'] as List);
+        return list
+            .map<StreamModel>((e) => StreamModel.fromJson(e as Map<String, dynamic>))
+            .toList();
+      },
+    );
+  }
+
+  Future<StreamModel> joinStream(String id) {
+    return ApiService().request(
+      httpMethod: HttpMethod.post,
+      uri: 'streams/$id/join',
+      parser: (res) => StreamModel.fromJson(res),
+    );
+  }
+}
